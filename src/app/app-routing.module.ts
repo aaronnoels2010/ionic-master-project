60994780da62599aba57ec969f7de0b090467221
aaronnoels <noels.aaron@gmail.com>
2020-03-21 17:11:27 +0100

    Setup pages
    All pages for the master project are created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,36 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: "", redirectTo: "auth", pathMatch: "full" },
+  {
+    path: "auth",
+    loadChildren: () => import("./auth/auth.module").then(m => m.AuthPageModule)
+  },
+  {
+    path: 'discover',
+    loadChildren: () => import('./places/discover/discover.module').then( m => m.DiscoverPageModule)
+  },
+  {
+    path: 'offers',
+    loadChildren: () => import('./places/offers/offers.module').then( m => m.OffersPageModule)
+  },
+  {
+    path: 'new-offer',
+    loadChildren: () => import('./places/offers/new-offer/new-offer.module').then( m => m.NewOfferPageModule)
+  },
+  {
+    path: 'edit-offer',
+    loadChildren: () => import('./places/offers/edit-offer/edit-offer.module').then( m => m.EditOfferPageModule)
+  },
+  {
+    path: 'offer-bookings',
+    loadChildren: () => import('./places/offers/offer-bookings/offer-bookings.module').then( m => m.OfferBookingsPageModule)
+  },
+  {
+    path: 'bookings',
+    loadChildren: () => import('./bookings/bookings.module').then( m => m.BookingsPageModule)
+  }
 ];
 
 @NgModule({
@@ -12,4 +39,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { DiscoverPipe } from './places/discover.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, DiscoverPipe],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
